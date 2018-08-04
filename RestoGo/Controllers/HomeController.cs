429dc7c3c8b9950740336f0905babe716963a9e3
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-08-04 15:18:51 -0400

    working Distance API

--- RestoGo/Controllers/HomeController.cs
+++ RestoGo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestoGo.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +12,29 @@ namespace RestoGo.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=H8N2K5&destinations=H8N2K7|H8P1E5&key=" + Globals.DistanceAPIkey;
+            try
+            {
+                string json = Api.getJson(url);
+                GoogleMatrixData goo = JsonConvert.DeserializeObject<GoogleMatrixData>(json);
+                if (goo.rows[0].elements[0].status != "OK") throw new Exception();
+                //tbGoogleDistanceTo.Content = goo.rows[0].elements[0].distance.text.ToString();
+                //tbGoogleTimeTo.Content = goo.rows[0].elements[0].duration.text.ToString();
+                //int.TryParse(goo.rows[0].elements[0].distance.value + "", out meterGoo[0]);
+                //int.TryParse(goo.rows[0].elements[0].duration.value + "", out secGoo[0]);
+                
+                Console.WriteLine("distance OK");
+                return View(goo);
+
+            }
+
+            catch (Exception)
+            {
+                //no distances found
+                Console.WriteLine("No distance!!!");
+                return View();
+            }
+            
         }
 
         public ActionResult About()
