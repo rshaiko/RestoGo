e6dacb94504fa15455baef8927e183449c976cdc
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-08-04 17:36:03 -0400

    get coord added

---
+++ RestoGo/API/GoogleCoordinates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoGo.API
+{
+    public class GoogleCoordinates
+    {
+        public Results [] results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Results
+    {
+        public Address_components [] address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+    }
+
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Address_components
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public string [] types { get; set; }
+
+    }
+}
--- RestoGo/Controllers/HomeController.cs
+++ RestoGo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using RestoGo.API;
+using RestoGo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,53 @@ namespace RestoGo.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        string url, json;
+
         public ActionResult Index()
         {
-            string url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=H8N2K5&destinations=H8N2K7|H8P1E5&key=" + Globals.DistanceAPIkey;
+
+
+            double[] userCoordinates = {0, 0};
+
+            if (User.Identity.IsAuthenticated) {
+                // get current user coordinates
+                string userName = System.Web.HttpContext.Current.User.Identity.Name;
+                var curUser = db.Users.Where(a => a.Email == userName).SingleOrDefault();
+                url = "http://maps.google.com/maps/api/geocode/json?address=" + curUser.Zip;
+                try
+                {
+                json = Api.getJson(url);
+                GoogleCoordinates coord = JsonConvert.DeserializeObject<GoogleCoordinates>(json);
+                if (coord.status != "OK") throw new Exception();
+                    userCoordinates[0] = coord.results[0].geometry.location.lat;
+                    userCoordinates[1] = coord.results[0].geometry.location.lng;
+
+                }
+                catch (Exception)
+                {
+                    //no coordinates found
+                    Console.WriteLine("Coordinates fail!!!");
+                    // using MONTREAL DOWNTOWN
+                    userCoordinates[0] = 45.7058381;
+                    userCoordinates[1] = -73.47426;
+
+                }
+            }
+            else
+            {
+                // using MONTREAL DOWNTOWN
+                userCoordinates[0] = 45.7058381;
+                userCoordinates[1] = -73.47426;
+            }
+            ViewBag.Coord = userCoordinates;
+
+
+
+            url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=H8N2K5&destinations=H8N2K7|H8P1E5&key=" + Globals.DistanceAPIkey;
             try
             {
-                string json = Api.getJson(url);
+                json = Api.getJson(url);
                 GoogleMatrixData goo = JsonConvert.DeserializeObject<GoogleMatrixData>(json);
                 if (goo.rows[0].elements[0].status != "OK") throw new Exception();
                 //tbGoogleDistanceTo.Content = goo.rows[0].elements[0].distance.text.ToString();
