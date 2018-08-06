9c54434f237290aabf269d6c61ae7e1698722872
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-08-05 21:26:56 -0400

    Search by location

--- RestoGo/Controllers/SearchController.cs
+++ RestoGo/Controllers/SearchController.cs
@@ -13,67 +13,225 @@ namespace RestoGo.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         string url, json;
+        double[] userCoordinates = { 0, 0 };
+        
+
         public ActionResult Index()
         {
             ViewBag.Message = "RestoGo - Advanced search";
-
-            double[] userCoordinates = { 0, 0 };
-
+            ViewBag.FormData = new string[]{ "","",""};
             if (User.Identity.IsAuthenticated)
             {
                 // get current user coordinates
                 string userName = System.Web.HttpContext.Current.User.Identity.Name;
                 var curUser = db.Users.Where(a => a.Email == userName).SingleOrDefault();
-                url = "http://maps.google.com/maps/api/geocode/json?address=" + curUser.Zip;
-                try
-                {
-                    json = Api.getJson(url);
-                    GoogleCoordinates coord = JsonConvert.DeserializeObject<GoogleCoordinates>(json);
-                    if (coord.status != "OK") throw new Exception();
-                    userCoordinates[0] = coord.results[0].geometry.location.lat;
-                    userCoordinates[1] = coord.results[0].geometry.location.lng;
-
-                }
-                catch (Exception)
-                {
-                    //no coordinates found
-                    Console.WriteLine("Coordinates fail!!!");
-                    // using MONTREAL DOWNTOWN
-                    userCoordinates[0] = 45.7058381;
-                    userCoordinates[1] = -73.47426;
-
-                }
+                ViewBag.UserAddress = new string[]{curUser.Address, curUser.City, curUser.Zip };
             }
             else
             {
-                // using MONTREAL DOWNTOWN
-                userCoordinates[0] = 45.7058381;
-                userCoordinates[1] = -73.47426;
+                ViewBag.UserAddress = null;
             }
-            ViewBag.Coord = userCoordinates;
+
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    // get current user coordinates
+            //    string userName = System.Web.HttpContext.Current.User.Identity.Name;
+            //    var curUser = db.Users.Where(a => a.Email == userName).SingleOrDefault();
+            //    ViewBag.CurUser = curUser;
+            //    url = "http://maps.google.com/maps/api/geocode/json?address=" + curUser.Zip;
+            //    try
+            //    {
+            //        json = Api.getJson(url);
+            //        GoogleCoordinates coord = JsonConvert.DeserializeObject<GoogleCoordinates>(json);
+            //        if (coord.status != "OK") throw new Exception();
+            //        userCoordinates[0] = coord.results[0].geometry.location.lat;
+            //        userCoordinates[1] = coord.results[0].geometry.location.lng;
+
+            //    }
+            //    catch (Exception)
+            //    {
+            //        //no coordinates found
+            //        Console.WriteLine("Coordinates fail!!!");
+            //        // using MONTREAL DOWNTOWN
+            //        userCoordinates[0] = 45.7058381;
+            //        userCoordinates[1] = -73.47426;
+
+            //    }
+            //}
+            //else
+            //{
+            //    // using MONTREAL DOWNTOWN
+            //    userCoordinates[0] = 45.7058381;
+            //    userCoordinates[1] = -73.47426;
+            //}
+            //ViewBag.Coord = userCoordinates;
 
 
 
-            url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=H8N2K5&destinations=H8N2K7|H8P1E5&key=" + Globals.DistanceAPIkey;
+            //url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=H8N2K5&destinations=H8N2K7|H8P1E5&key=" + Globals.DistanceAPIkey;
+            //try
+            //{
+            //    json = Api.getJson(url);
+            //    GoogleMatrixData goo = JsonConvert.DeserializeObject<GoogleMatrixData>(json);
+            //    if (goo.rows[0].elements[0].status != "OK") throw new Exception();
+            //    //tbGoogleDistanceTo.Content = goo.rows[0].elements[0].distance.text.ToString();
+            //    //tbGoogleTimeTo.Content = goo.rows[0].elements[0].duration.text.ToString();
+            //    //int.TryParse(goo.rows[0].elements[0].distance.value + "", out meterGoo[0]);
+            //    //int.TryParse(goo.rows[0].elements[0].duration.value + "", out secGoo[0]);
+
+            //    Console.WriteLine("distance OK");
+            //    return View(goo);
+
+            //}
+
+            //catch (Exception)
+            //{
+            //    //no distances found
+            //    Console.WriteLine("No distance!!!");
+            //    return View();
+            //}
+            ViewBag.SearchError = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string addr, string city, string prov, string zip)
+        {
+            string[] formData = { addr, city, zip };
+            ViewBag.FormData = formData;
+            ViewBag.SearchError = "";
+            ViewBag.FormattedAddress = "";
+            double[] searchCoordinates = { 0, 0 };
+            string searchAddress="";
+            if (zip != "")
+            {
+                searchAddress = zip;
+            }
+            else if (addr != "" || city != "")
+            {
+                searchAddress = addr + " " + city + " " + prov;
+                searchAddress = searchAddress.Trim();
+            }
+            else
+            {
+                ViewBag.SearchError = "Error - no address submitted";
+                //no coordinates found
+                return View();
+            }
+
+            url = "http://maps.google.com/maps/api/geocode/json?address=" + searchAddress;
             try
             {
                 json = Api.getJson(url);
-                GoogleMatrixData goo = JsonConvert.DeserializeObject<GoogleMatrixData>(json);
-                if (goo.rows[0].elements[0].status != "OK") throw new Exception();
-                //tbGoogleDistanceTo.Content = goo.rows[0].elements[0].distance.text.ToString();
-                //tbGoogleTimeTo.Content = goo.rows[0].elements[0].duration.text.ToString();
-                //int.TryParse(goo.rows[0].elements[0].distance.value + "", out meterGoo[0]);
-                //int.TryParse(goo.rows[0].elements[0].duration.value + "", out secGoo[0]);
+                GoogleCoordinates coord = JsonConvert.DeserializeObject<GoogleCoordinates>(json);
+                if (coord.status != "OK") throw new Exception();
+                searchCoordinates[0] = coord.results[0].geometry.location.lat;
+                searchCoordinates[1] = coord.results[0].geometry.location.lng;
+                ViewBag.FormattedAddress = coord.results[0].formatted_address;
 
-                Console.WriteLine("distance OK");
-                return View(goo);
+            }
+            catch (Exception)
+            {
+                ViewBag.SearchError = "Error - Location not found, please verify the address";
+                //no coordinates found
+                return View();
+                Console.WriteLine("Coordinates fail!!!");
+                // using MONTREAL DOWNTOWN
+                searchCoordinates[0] = 45.7058381;
+                searchCoordinates[1] = -73.47426;
+            }
+
+
+
+            // GET: Restaurant
+
+            url = "https://developers.zomato.com/api/v2.1/geocode?lat="+ searchCoordinates[0] + "&lon="+ searchCoordinates[1] + "&apikey=" + "4d72030018edd126975c251c29c50f70";
+            List<Restaurant> list = new List<Restaurant>();
+            try
+            {
+                string json = Api.getJson(url);
+                dynamic obj = JsonConvert.DeserializeObject(json);
+
+                dynamic obj2 = obj.nearby_restaurants;
+                foreach (var item in obj2)
+                {
+                    Restaurant rest = new Restaurant();
+                    if (item.restaurant != null)
+                    {
+                        rest.Name = item.restaurant.name;
+                        rest.Address = item.restaurant.location.address;
+                        rest.City = item.restaurant.location.city;
+                        rest.ZipCode = item.restaurant.location.zipcode;
+                        rest.URL = item.restaurant.url;
+                        rest.Rating = item.restaurant.user_rating.rating_text;
+                        rest.Ciusine = item.restaurant.cuisines;
+                        rest.AverageCost = item.restaurant.average_cost_for_two;
+                        rest.Color = item.restaurant.user_rating.rating_color;
+                        rest.AggregateRating = item.restaurant.user_rating.aggregate_rating;
+                        rest.Votes = item.restaurant.user_rating.votes;
+                        rest.Latitude = item.restaurant.location.latitude;
+                        rest.Longitude = item.restaurant.location.longitude;
+                        list.Add(rest);
+                    }
+
+                }
+
+                //Adding time and distance
+                string[] arrDest = new string[list.Count];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    arrDest[i] = list[i].ZipCode;
+                }
+                string destinations = String.Join("|", arrDest);
+
+                string zipFrom = "H2Z1A7";
+                if (User.Identity.IsAuthenticated)
+                {
+                    string userName = System.Web.HttpContext.Current.User.Identity.Name;
+                    var curUser = db.Users.Where(a => a.Email == userName).SingleOrDefault();
+                    zipFrom = curUser.Zip;
+                }
+                url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+zipFrom+"&destinations=" + destinations + "&key=" + Globals.DistanceAPIkey;
+                try
+                {
+                    json = Api.getJson(url);
+                    GoogleMatrixData goo = JsonConvert.DeserializeObject<GoogleMatrixData>(json);
+                    if (goo.status != "OK") throw new Exception();
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (goo.rows[0].elements[i].status != "OK")
+                        {
+                            list[i].Distance = "n/d";
+                            list[i].TimeToGetTo = "n/d";
+                        }
+                        else
+                        {
+                            list[i].Distance = goo.rows[0].elements[i].distance.text.ToString();
+                            list[i].TimeToGetTo = goo.rows[0].elements[i].duration.text.ToString();
+
+                        }
+                    }
+                    return View(list);
+
+                }
+
+                catch (Exception)
+                {
+                    //no distances found
+                    Console.WriteLine("No distance!!!");
+                    return View();
+                }
+
+
+                Console.WriteLine("OK");
+                return View(list);
 
             }
 
             catch (Exception)
             {
-                //no distances found
-                Console.WriteLine("No distance!!!");
+
+                Console.WriteLine("Error");
                 return View();
             }
         }
